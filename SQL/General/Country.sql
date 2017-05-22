f09facc3472b2f61bbef449ee2c5a0c480d358b0
Bryan Alejandro Cubillos Prieto <bryant_alejandro@hotmail.com>
2017-05-22 17:23:47 -0500

    cambio de sql

--- SQL/General/Country.sql
+++ SQL/General/Country.sql
@@ -1,1 +1,26 @@
 -- This file
+
+USE [SHC]
+GO
+
+/****** Object:  Table [dbo].[Country]    Script Date: 22/05/2017 5:22:31 p.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Country](
+	[idCountry] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
